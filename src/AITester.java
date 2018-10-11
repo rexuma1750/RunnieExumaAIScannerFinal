b935409aa9b2122dd788b4eda69018655356c906
rexuma1750 <rexuma1750@bths.edu>
2018-10-11 08:41:49 -0400

    Initial commit

---
+++ src/AITester.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AITester {
+    public static void main(String[]args)
+    {
+        System.out.println("Pick a number between 1 and 100.");
+        Scanner input = new Scanner(System.in);
+        int min = 1;
+        int max = 100;
+        while (!guess.equals()))
+        {
+
+
+
+    }
+}
