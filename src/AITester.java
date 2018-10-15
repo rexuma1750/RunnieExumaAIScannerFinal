2e56c67b7f766355f0a9a753c6d30c1b68371bd8
runnie <rexuma1750@bths.edu>
2018-10-14 20:51:02 -0400

    updated

--- src/AITester.java
+++ src/AITester.java
@@ -4,11 +4,35 @@ public class AITester {
     public static void main(String[]args)
     {
         System.out.println("Pick a number between 1 and 100.");
+        System.out.println("Indicate 'higher' or 'lower' to get closer to the correct guess.");
         Scanner input = new Scanner(System.in);
+        String input = "";
+
         int min = 1;
         int max = 100;
-        while (!guess.equals()))
+        int numberOfGuesses = 1;
+        int guessNumber = 24;
+
+        while (!(input.equals("Correct! That is the right number.")))
         {
+            System.out.println("Your guess is"+guessNumber+".");
+            input = input.nextLine();
+            if (input.equals("Guess higher."))
+            {
+                min = guessNumber;
+                guessNumber = (max+min)/2;
+                numberOfGuesses++;
+            }
+            else if (input.equals("Guess lower."))
+            {
+                max = guessNumber;
+                guessNumber = (max+min)/2;
+                numberOfGuesses++;
+            }
+
+        }
+        System.out.println("I guessed correctly in"+numberOfGuesses+".");
+
 
 
 
